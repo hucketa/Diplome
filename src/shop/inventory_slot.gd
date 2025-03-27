c2785933dfea2c27806f6a0149f406dc1f38860e
hucketa <gunenkoym@gmail.com>
2025-03-27 10:31:45 +0200

    Правки магазина
    1) Дописал рааботу с инвентарем
    2) Пофиксил локализацию
    Надо сделать:
    1) Запуск магазина по окончанию волны
    2) Кнопки

---
+++ src/shop/inventory_slot.gd
@@ -0,0 +1,48 @@
+extends Button
+
+var slot_index: int
+var inventory: Node
+var weapon_data: WeaponData
+var player: CharacterBody2D
+signal inventory_changed 
+
+@onready var texture_rect: TextureRect = $VBoxContainer/TextureRect
+@onready var name_label: Label = $VBoxContainer/Label
+@onready var confirm_dialog: ConfirmationDialog = $ConfirmationDialog
+
+func set_player(a: CharacterBody2D):
+	player = a
+
+func update_display(data: WeaponData, index: int, inv: Node):
+	weapon_data = data
+	slot_index = index
+	inventory = inv
+	if data == null:
+		texture_rect.texture = null
+		name_label.text = ""
+		return
+	texture_rect.texture = data.icon if data.icon else null
+	name_label.text = data.weapon_name
+
+func _ready() -> void:
+	update_display(null, 0,null)
+	update_display(null, 1,null)
+	update_display(null, 2,null)
+	update_display(null, 3,null)
+
+func _on_confirmed():
+	if inventory and inventory.has_method("remove_weapon"):
+		inventory.remove_weapon(slot_index)
+		player.stats.__coins += weapon_data.price
+		emit_signal("inventory_changed")
+		texture_rect.texture = null
+		name_label.text = ""
+		weapon_data = null
+
+
+func _on_pressed() -> void:
+	if weapon_data != null:
+		confirm_dialog.popup_centered()
+
+func _on_confirmation_dialog_canceled() -> void:
+	print("hhh")
--- src/shop/shop.gd
+++ src/shop/shop.gd
@@ -1,14 +1,14 @@
 extends Control
 
 @onready var slot_1_buy: Button = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/Slot_1_buy"
-@onready var slot_1_buy_container: HBoxContainer = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1"
-@onready var slot_2_buy_container: HBoxContainer = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot2"
-@onready var rarity_1: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/Slot_1_buy/HBoxContainer/Slot 1/HBoxContainer/Rarity"
+@onready var slot_2_buy: Button = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot2/Slot2_buy"
+
+@onready var texture_rect_1: TextureRect = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/TextureRect/TextureRect"
 @onready var weapon_name_1: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/Slot_1_buy/HBoxContainer/Slot 1/HBoxContainer/WeaponName"
+@onready var rarity_1: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/Slot_1_buy/HBoxContainer/Slot 1/HBoxContainer/Rarity"
 @onready var price_1: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/Slot_1_buy/HBoxContainer/Slot 1/HBoxContainer/Price"
 @onready var description_1: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/Slot_1_buy/HBoxContainer/Slot 1/VBoxContainer/Description"
-@onready var texture_rect_1: TextureRect = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot1/TextureRect/TextureRect"
-@onready var slot_2_buy: Button = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot2/Slot2_buy"
+
 @onready var texture_rect_2: TextureRect = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot2/TextureRect/TextureRect"
 @onready var weapon_name_2: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot2/Slot2_buy/HBoxContainer/Slot 1/HBoxContainer/WeaponName"
 @onready var rarity_2: Label = $"VBoxContainer/BuySlots and Stats/Buy_slots/BuySlot2/Slot2_buy/HBoxContainer/Slot 1/HBoxContainer/Rarity"
@@ -19,47 +19,42 @@ extends Control
 @onready var inventory := player.get_node("InventoryUi")
 @onready var player_stats := player.get_node("stats")
 
-@onready var health_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/Health/Value"
-@onready var damage_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/Damage/Value"
-@onready var AS_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/AS/Value"
-@onready var Crit_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/Crit_chance/Value"
-@onready var Level_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/Level/Value"
-@onready var Exp_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/Experience/Value"
-@onready var Balance_value: Label = $"VBoxContainer/BuySlots and Stats/Stats/Gold/Value"
+@onready var slot_1: Button = $"VBoxContainer/Inventory and Buttons/Inventory/Inventory/Slot1"
+@onready var slot_2: Button = $"VBoxContainer/Inventory and Buttons/Inventory/Inventory/Slot2"
+@onready var slot_3: Button = $"VBoxContainer/Inventory and Buttons/Inventory/Inventory/Slot 3"
+@onready var slot_4: Button = $"VBoxContainer/Inventory and Buttons/Inventory/Inventory/Slot4"
 
 var buy_slot_weapons: Array = []
 
-func fill_stats() -> void:
-	health_value.text = str(player_stats.__max_health)
-	damage_value.text = str(player_stats.__damage)
-	AS_value.text = str(player_stats.__attack_speed)
-	Crit_value.text = str(player_stats.__crit_chance*100)
-	Level_value.text = str(player_stats.__level)
-	Exp_value.text = str(player_stats.__experience_to_level_up)
-	Balance_value.text = str(player_stats.__coins)
-
 func _ready():
-	TranslationServer.set_locale("en")
+	TranslationServer.set_locale("uk")
+	slot_1.set_player(player)
+	slot_2.set_player(player)
+	slot_3.set_player(player)
+	slot_4.set_player(player)
 	populate_buy_slots()
 	connect_buy_signals()
+	populate_inventory_ui()
 	fill_stats()
 
 func populate_buy_slots():
 	buy_slot_weapons.clear()
 	var weapons = WeaponDB.get_random(2)
+
 	var weapon_1 = weapons[0]
 	buy_slot_weapons.append(weapon_1)
 	texture_rect_1.texture = weapon_1.icon
 	weapon_name_1.text = weapon_1.weapon_name
 	rarity_1.text = weapon_1.rarity
-	price_1.text = str(weapon_1.price)+" "+tr("COINS")
+	price_1.text = str(weapon_1.price) + " " + tr("COINS")
 	description_1.text = weapon_1.description
+
 	var weapon_2 = weapons[1]
 	buy_slot_weapons.append(weapon_2)
 	texture_rect_2.texture = weapon_2.icon
 	weapon_name_2.text = weapon_2.weapon_name
 	rarity_2.text = weapon_2.rarity
-	price_2.text = str(weapon_2.price)+" "+tr("COINS")
+	price_2.text = str(weapon_2.price) + " " + tr("COINS")
 	description_2.text = weapon_2.description
 
 func connect_buy_signals():
@@ -73,7 +68,10 @@ func _on_buy_slot_pressed(slot_index: int):
 		if free_index != -1:
 			player_stats.__coins -= weapon.price
 			inventory.add_weapon_from_data(weapon, free_index)
-			print("Куплено зброю:", weapon.weapon_name)
+			var ui_slots = [slot_1, slot_2, slot_3, slot_4]
+			var ui_slot = ui_slots[free_index]
+			ui_slot.update_display(weapon, free_index, inventory)
+			fill_stats()
 		else:
 			print("Немає вільних слотів в інвентарі.")
 	else:
@@ -81,6 +79,28 @@ func _on_buy_slot_pressed(slot_index: int):
 
 func find_first_free_inventory_slot() -> int:
 	for i in inventory.slots.size():
-		if inventory.slots[i].weapon == null:
+		var data = inventory.slots[i]
+		if data.get("weapon", null) == null:
 			return i
 	return -1
+
+func populate_inventory_ui() -> void:
+	var ui_slots := [slot_1, slot_2, slot_3, slot_4]
+	for i in range(inventory.slots.size()):
+		var ui_slot = ui_slots[i]
+		var slot_data = inventory.slots[i]
+		var weapon_data = slot_data.get("weapon_data", null)
+		if slot_data.weapon != null:
+			ui_slot.update_display(weapon_data, i, inventory)
+		else:
+			ui_slot.update_display(null, i, inventory)
+		fill_stats()
+
+func fill_stats() -> void:
+	$"VBoxContainer/BuySlots and Stats/Stats/Health/Value".text = str(player_stats.__max_health)
+	$"VBoxContainer/BuySlots and Stats/Stats/Damage/Value".text = str(player_stats.__damage)
+	$"VBoxContainer/BuySlots and Stats/Stats/AS/Value".text = str(player_stats.__attack_speed)
+	$"VBoxContainer/BuySlots and Stats/Stats/Crit_chance/Value".text = str(player_stats.__crit_chance * 100)
+	$"VBoxContainer/BuySlots and Stats/Stats/Level/Value".text = str(player_stats.__level)
+	$"VBoxContainer/BuySlots and Stats/Stats/Experience/Value".text = str(player_stats.__experience_to_level_up)
+	$"VBoxContainer/BuySlots and Stats/Stats/Gold/Value".text = str(player_stats.__coins)
